c81ba6cd95abbf982da3feabf9b243ab08af3b6c
muhammed mishal <mishalp789@gmail.com>
2026-07-16 15:00:17 +0530

    Complete project foundation with Flyway and JPA auditing

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    full_name VARCHAR(150),
+    role VARCHAR(30) NOT NULL,
+    enabled BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+    );
